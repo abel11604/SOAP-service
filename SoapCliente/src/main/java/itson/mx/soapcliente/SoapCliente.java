84fd84759003e000e80aa58a708c19b0aa185432
abel11604 <abelcorona.quintero@gmail.com>
2025-03-06 00:24:12 -0700

    Cliente jala

--- SoapCliente/src/main/java/itson/mx/soapcliente/SoapCliente.java
+++ SoapCliente/src/main/java/itson/mx/soapcliente/SoapCliente.java
@@ -4,6 +4,11 @@
 
 package itson.mx.soapcliente;
 
+import com.itson.soapcliente.SOAPImpl;
+import com.itson.soapcliente.SOAPImpl_Service;
+import com.itson.soapcliente.User;
+import java.util.List;
+
 
 
 
@@ -14,6 +19,32 @@ package itson.mx.soapcliente;
 public class SoapCliente {
 
     public static void main(String[] args) {
+         // Crear el cliente del servicio
+        SOAPImpl_Service service = new SOAPImpl_Service();
+        SOAPImpl port = service.getSOAPImplPort();
+
+        //lista de usuarios antes de carlos
+        System.out.println("Antes de carlos");
+        List<User> usuariosA = port.getUsers();
+        for (User u : usuariosA) {
+            System.out.println("Usuario: " + u.getName() + " - Username: " + u.getUsername());
+        }
+        // Agregar un nuevo usuario al servicio SOAP
+        User nuevoUsuario = new User();
+        nuevoUsuario.setName("Carlos");
+        nuevoUsuario.setUsername("carlos123");
+
+        System.out.println("Agregando usuario: " + nuevoUsuario.getName());
+        port.addUser(nuevoUsuario);
         
+        // Obtener la lista de usuarios desde el servicio SOAP
+       
+        List<User> usuarios = port.getUsers();
+
+        // Mostrar la lista de usuarios obtenida
+        for (User u : usuarios) {
+            System.out.println("Usuario: " + u.getName() + " - Username: " + u.getUsername());
+        }
+    
     }
 }
